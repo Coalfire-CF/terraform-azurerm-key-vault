9db401f0cc42fa3fde9d9a7d00ca34bf75e6c7e3
Douglas Francis <Douglas@PoshOps.io>
2023-02-24 15:27:56 -0600

    kv diag fix
    changed kv diag from .. source to git module source

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_key_vault" "key-vault" {
 }
 
 module "diag" {
-  source                = "../coalfire-diagnostic/"
+  source                = "github.com/Coalfire-CF/Ace-Azure-Keyvault"
   diag_log_analytics_id = var.diag_log_analytics_id
   resource_id           = azurerm_key_vault.key-vault.id
   resource_type         = "kv"
